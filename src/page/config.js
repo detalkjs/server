80ba73ab9945e68a4e969294da739c09a5baa8b5
CKY <awa@outlook.ie>
2023-01-06 11:58:45 +0800

    :sparkles: 新增快速配置

--- src/page/config.js
+++ src/page/config.js
@@ -7,6 +7,10 @@ module.exports = `<!DOCTYPE html>
     <title>Config</title>
 </head>
 <body>
-    
+    <script>
+        let host = window.location.origin;
+        let url = prompt('Please enter your dashboard URL.', 'https://detalk-dash.netlify.app') || 'https://detalk-dash.netlify.app';
+        document.body.innerHTML = '<a href="' + url + '/login.html?url=' + encodeURIComponent(window.location.origin) + '">Go to Dashboard</a>';
+    </script>
 </body>
 </html>`;
