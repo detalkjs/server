ca9ae6e97e313877c92a7aff6d12addac1c4a40d
CKY <awa@outlook.ie>
2023-01-10 12:02:48 +0800

    :art: 支持 Img 标签

--- index.js
+++ index.js
@@ -172,7 +172,23 @@ app.put('/_api/comment', async (req, res) => {
             if (nickname.length >= 15 || content.length >= 500 || email.length >= 50 || url.length >= 100) throw "Nickname, email, url or content is too long.";
             url = textconvert(url) || "";
             nickname = textconvert(nickname);
-            content = sanitizeHtml(marked.parse(content));
+            content = sanitizeHtml(marked.parse(content), {
+                allowedTags: [
+                    "address", "article", "aside", "footer", "header", "h1", "h2", "h3", "h4",
+                    "h5", "h6", "hgroup", "main", "nav", "section", "blockquote", "dd", "div",
+                    "dl", "dt", "figcaption", "figure", "hr", "li", "main", "ol", "p", "pre",
+                    "ul", "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data", "dfn",
+                    "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp",
+                    "small", "span", "strong", "sub", "sup", "time", "u", "var", "wbr", "caption",
+                    "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "img"
+                ],
+                allowedAttributes: {
+                    a: [ 'href', 'name', 'target' ],
+                    // We don't currently allow img itself by default, but
+                    // these attributes would make sense if we did.
+                    img: [ 'src', 'srcset', 'alt', 'title', 'width', 'height' ]
+                },
+            });
 
 
             // reCAPTCHA 验证
